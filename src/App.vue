e402311dfce8ae8db7ccae5911d5e93ddc1068bf
John Eckert <johnteckert@gmail.com>
2018-12-05 11:53:53 -0500

    p5 working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <vue-p5 v-on="{setup}"></vue-p5>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import VueP5 from "vue-p5";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    vueP5: VueP5
+  },
+  methods: {
+    setup(sketch) {
+      sketch.background("green");
+      sketch.text("Hello p5!", 20, 20);
+    }
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
 }
 </style>
