a931773e2d87376ffc566bf52323e54c52df2870
John Eckert <johnteckert@gmail.com>
2018-12-05 15:08:04 -0500

    random line drawing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <vue-p5 v-on="{setup}"></vue-p5>
+    <vue-p5 @setup="setup" @draw="draw"></vue-p5>
   </div>
 </template>
 
@@ -12,10 +12,51 @@ export default {
   components: {
     vueP5: VueP5
   },
+  data() {
+    return {
+      width: 0,
+      height: 0,
+      t: 100
+    };
+  },
+  created() {
+    //get window dimensions live
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.handleResize);
+  },
   methods: {
+    handleResize() {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+    },
     setup(sketch) {
-      sketch.background("green");
-      sketch.text("Hello p5!", 20, 20);
+      sketch.createCanvas(this.width, this.height);
+      sketch.background(112, 128, 144);
+      sketch.stroke(0, 18);
+      sketch.noFill();
+    },
+    draw(sketch) {
+      let x1 = this.width * sketch.noise(this.t + this.width * 0);
+      let x2 = this.width * sketch.noise(this.t + this.width * 5);
+      let x3 = this.width * sketch.noise(this.t + this.width * 10);
+      let x4 = this.width * sketch.noise(this.t + this.width * 20);
+      let y1 = this.height * sketch.noise(this.t + this.height * 0);
+      let y2 = this.height * sketch.noise(this.t + this.height * 5);
+      let y3 = this.height * sketch.noise(this.t + this.height * 10);
+      let y4 = this.height * sketch.noise(this.t + this.height * 20);
+
+      sketch.bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+
+      this.t += 0.5;
+
+      // clear the background every 500 frames using mod (%) operator
+      if (sketch.frameCount % 5000 == 0) {
+        //will it ever crash if I don't clear?
+        // sketch.background(112, 128, 144);
+      }
     }
   }
 };
