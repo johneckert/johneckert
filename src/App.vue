d95171fcd87f46b345d38d7650598b6362c2db55
John Eckert <johnteckert@gmail.com>
2018-12-05 21:56:06 -0500

    added frame rate variable

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default {
     return {
       width: 0,
       height: 0,
+      fr: 100, //default is 60
       t: 700,
       bgColor: "rgba(112, 128, 144, 0)",
       strokeColor: 0,
@@ -43,21 +44,24 @@ export default {
       sketch.background(this.bgColor);
       sketch.stroke(this.strokeColor, this.strokeAlpha);
       sketch.noFill();
+      sketch.frameRate(this.fr);
     },
     draw(sketch) {
       let x1 = this.width * sketch.noise(this.t + this.width * 70); // * 0 here will make it a diagonal
-      let x2 = this.width * sketch.noise(this.t + this.width * 20);
-      let x3 = this.width * sketch.noise(this.t + this.width * 10);
-      let x4 = this.width * sketch.noise(this.t + this.width * 50);
       let y1 = this.height * sketch.noise(this.t + this.height * 40); // * 0 here will make it a diagonal
+
+      let x2 = this.width * sketch.noise(this.t + this.width * 20);
       let y2 = this.height * sketch.noise(this.t + this.height * 50);
+
+      let x3 = this.width * sketch.noise(this.t + this.width * 80);
       let y3 = this.height * sketch.noise(this.t + this.height * 10);
+
+      let x4 = this.width * sketch.noise(this.t + this.width * 50);
       let y4 = this.height * sketch.noise(this.t + this.height * 20);
 
       sketch.bezier(x1, y1, x2, y2, x3, y3, x4, y4);
 
       this.t += 0.5;
-
       //stop drawing after 5000 frames
       if (sketch.frameCount >= 8000) {
         sketch.frameRate(0);
@@ -94,7 +98,7 @@ body {
   font-family: "Anonymous Pro", monospace;
   font-size: 1rem;
   color: rgb(112, 128, 144);
-  left: 45%;
+  left: 44%;
   top: 48%;
 }
 </style>
